a36fa5d194e45ef57d0a2d4c5c743294ec6de4bd
Pere Palacín <perepalacin@gmail.com>
2025-10-26 23:17:38 +0100

    feat: add c stack

--- c/src/main.c
+++ c/src/main.c
@@ -1,17 +1,23 @@
 #include "./common/common.h"
 #include "./chunk/chunk.h"
 #include "./debug/debug.h"
+#include "./vm/vm.h"
 int main(int argc, const char *argv[])
 {
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
     int constant = addConstant(&chunk, 1.2);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
+    writeChunk(&chunk, OP_NEGATE, 123);
     writeChunk(&chunk, OP_RETURN, 123);
 
     disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
 
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
---
+++ c/src/vm/vm.h
@@ -0,0 +1,32 @@
+#ifndef clox_vh_h
+#define clox_vh_h
+
+#include <stdio.h>
+
+#include "../chunk/chunk.h"
+#include "../value/value.h"
+
+#define STACK_MAX 256
+
+typedef struct
+{
+    Chunk *chunk;
+    uint8_t *ip;
+    Value stack[STACK_MAX];
+    Value *stackTop
+} VM;
+
+typedef enum
+{
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR,
+} InterpretResult;
+
+void initVM();
+void freeVM();
+InterpretResult interpret(Chunk *chunk);
+void push(Value value);
+Value pop();
+
+#endif

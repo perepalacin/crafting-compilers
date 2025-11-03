29350dff277e37826fc13a6f0faf4cfa15c452e0
Pere Palacín <perepalacin@gmail.com>
2025-11-03 23:31:54 +0100

    add literal expressions to compiler

--- c/src/compiler/compiler.c
+++ c/src/compiler/compiler.c
@@ -13,7 +13,29 @@ typedef struct
     bool panicMode;
 } Parser;
 
+typedef enum
+{
+    PREC_NONE,
+    PREC_ASSIGNMENT,
+    PREC_OR,
+    PREC_AND,
+    PREC_EQUALITY,
+    PREC_COMPARISON,
+    PREC_TERM,
+    PREC_FACTOR,
+    PREC_UNARY,
+    PREC_CALL,
+    PREC_PRIMARY
+} Precedence;
+
 Parser parser;
+
+Chunk *compilingChunk;
+
+static Chunk *currentChunk()
+{
+    return compilingChunk;
+}
 
 static void errorAt(Token *token, const char *message)
 {
@@ -74,13 +96,74 @@ static void consume(TokenType type, const char *message)
     errorAtCurrent(message);
 }
 
+static void emitBytes(uint8_t byte1, uint8_t byte2)
+{
+    emitByte(byte1);
+    emitByte(byte2);
+}
+
+static void emitReturn()
+{
+    emitByte(OP_RETURN);
+}
+
+static void endCompiler()
+{
+    emitReturn();
+}
+
+static uint8_t makeConstant(Value value)
+{
+    int constant = addConstant(currentChunk(), value);
+    if (constant > UINT8_MAX)
+    {
+        error("Too many constants in one chunk.");
+        return 0;
+    }
+
+    return (uint8_t)constant;
+}
+
+static void emitConstant(Value value)
+{
+    emitBytes(OP_CONSTANT, makeConstant(value));
+}
+
+static void number()
+{
+    double value = strtod(parser.previous.start, NULL);
+    emitConstant(value);
+}
+
+static void grouping()
+{
+    expression();
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
+}
+
+static void unary()
+{
+    TokenType operatorType = parser.previous.type;
+    expression();
+    switch (operatorType)
+    {
+    case TOKEN_MINUS:
+        emitByte(OP_NEGATE);
+        break;
+    default:
+        return;
+    }
+}
+
 void compile(const char *source, Chunk *chunk)
 {
     initScanner(source);
+    compilingChunk = chunk;
     parser.hadError = false;
     parser.panicMode = false;
     advance();
     expression();
     consume(TOKEN_EOF, "Expect end of expression.");
+    endCompiler();
     return !parser.hadError;
 }
